f6c5b7e1e793618692ef7dd2e9a938d63d4734cb
Ronan <liruonan@hotmail.com>
2020-12-30 22:44:16 +0800

    style(reactive.spec.ts): 中文标注理解

--- packages/reactivity/__tests__/reactive.spec.ts
+++ packages/reactivity/__tests__/reactive.spec.ts
@@ -4,6 +4,10 @@ import { mockWarn } from '@vue/runtime-test'
 describe('reactivity/reactive', () => {
   mockWarn()
 
+  // reactived-Object
+  // 仅具有原始Object的keys，
+  // 且，get-key值类型的value保持不变，
+  // 且，原型链也可追溯到key
   test('Object', () => {
     const original = { foo: 1 }
     const observed = reactive(original)
@@ -18,6 +22,11 @@ describe('reactivity/reactive', () => {
     expect(Object.keys(observed)).toEqual(['foo'])
   })
 
+  // reactived-Array
+  // 内部Items 也具有reactived特性
+  // 且，get-key值类型的value保持不变
+  // 且，具有Array的index，例如：0 in reactived-Array
+  // 且，具有原始的keys，即为index
   test('Array', () => {
     const original = [{ foo: 1 }]
     const observed = reactive(original)
@@ -33,6 +42,10 @@ describe('reactivity/reactive', () => {
     expect(Object.keys(observed)).toEqual(['0'])
   })
 
+  // 浅cloned--reactived-Array
+  // 内部items，也具有reactived特性
+  // cloned-item === reactived-item
+  // cloned-item !== array-item
   test('cloned reactive Array should point to observed values', () => {
     const original = [{ foo: 1 }]
     const observed = reactive(original)
@@ -42,6 +55,10 @@ describe('reactivity/reactive', () => {
     expect(clone[0]).toBe(observed[0])
   })
 
+  // nested 嵌套的reactives
+  // 嵌套内的对象也具备reactived特性
+  // 嵌套内的数组也具备reactived特性
+  // 嵌套内的数组的item也具备reactived特性
   test('nested reactives', () => {
     const original = {
       nested: {
@@ -55,6 +72,8 @@ describe('reactivity/reactive', () => {
     expect(isReactive(observed.array[0])).toBe(true)
   })
 
+  // 针对Object，通过observed-proxy 赋值，对于原始对象同等有效
+  // = 直接赋值，delete 操作
   test('observed value should proxy mutations to original (Object)', () => {
     const original: any = { foo: 1 }
     const observed = reactive(original)
@@ -68,6 +87,9 @@ describe('reactivity/reactive', () => {
     expect('foo' in original).toBe(false)
   })
 
+  // 针对Array,通过observed-proxy赋值，对于原始arr同等有效
+  // index操作赋值、push() 操作赋值
+  // point1：arr = [1,2]; delete arr[0]; arr长度依然是2，index0会是一个empty
   test('observed value should proxy mutations to original (Array)', () => {
     const original: any[] = [{ foo: 1 }, { bar: 2 }]
     const observed = reactive(original)
@@ -87,6 +109,9 @@ describe('reactivity/reactive', () => {
     expect(original[2]).toBe(value)
   })
 
+  // 给observed赋值一个非reactived对象值raw
+  // 那么，observed.raw 具有reactived特性
+  // 同时，observed.raw !== raw
   test('setting a property with an unobserved value should wrap with reactive', () => {
     const observed = reactive<{ foo?: object }>({})
     const raw = {}
@@ -95,6 +120,7 @@ describe('reactivity/reactive', () => {
     expect(isReactive(observed.foo)).toBe(true)
   })
 
+  // 代理一个已经是代理的对象，那么新代理 === 旧代理
   test('observing already observed value should return same Proxy', () => {
     const original = { foo: 1 }
     const observed = reactive(original)
@@ -102,6 +128,7 @@ describe('reactivity/reactive', () => {
     expect(observed2).toBe(observed)
   })
 
+  // 2个代理 分别取代理同一对象，那么代理1 === 代理2
   test('observing the same value multiple times should return same Proxy', () => {
     const original = { foo: 1 }
     const observed = reactive(original)
@@ -109,6 +136,7 @@ describe('reactivity/reactive', () => {
     expect(observed2).toBe(observed)
   })
 
+  // 代理间的赋值操作，不能污染原始的对象
   test('should not pollute original object with Proxies', () => {
     const original: any = { foo: 1 }
     const original2 = { bar: 2 }
@@ -119,6 +147,8 @@ describe('reactivity/reactive', () => {
     expect(original.bar).toBe(original2)
   })
 
+  // toRaw()方法得到原始值
+  // reactived-Object === Object
   test('unwrap', () => {
     const original = { foo: 1 }
     const observed = reactive(original)
@@ -126,6 +156,9 @@ describe('reactivity/reactive', () => {
     expect(toRaw(original)).toBe(original)
   })
 
+  // 不可observable的值
+  // number, string, boolean, null, undefined, symbol
+  // 内建对象 同样的
   test('non-observable values', () => {
     const assertValue = (value: any) => {
       reactive(value)
@@ -157,6 +190,7 @@ describe('reactivity/reactive', () => {
     expect(reactive(d)).toBe(d)
   })
 
+  // 通过markNonReactive 来指定某个特性不被reactived
   test('markNonReactive', () => {
     const obj = reactive({
       foo: { a: 1 },
